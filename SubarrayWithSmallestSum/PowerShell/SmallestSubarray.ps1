b2020c3b225886849ea62d0669124099f0928bec
Alexander Drogin <adrogin@outlook.com>
2023-03-26 00:30:43 +0000

    Smallest subarray kata

---
+++ SubarrayWithSmallestSum/PowerShell/SmallestSubarray.ps1
@@ -0,0 +1,81 @@
+﻿class ArrayFrame {
+    [int]$Position
+    [int]$Length
+    [int]$Sum
+}
+
+function InitRandomArray {
+    Param(
+        [int]$length,
+        [int]$maxValue
+    )
+
+    $array = @()
+    for ($i = 0; $i -lt $length; $i++) {
+        $array += (Get-Random -Maximum $maxValue)
+    }
+
+    return $array
+}
+
+function CalcArraySum {
+    Param(
+        [int[]]$array
+    )
+
+    if ($array.Length -eq 1) {
+        return $array[0]
+    }
+
+    return (CalcArraySum $array[1..($array.Length - 1)]) + $array[0]
+}
+
+function FindMinRange {
+    Param(
+        [ArrayFrame]$minFrame,
+        [ArrayFrame]$currFrame
+    )
+
+    if ($currFrame.Position -gt $range.Count - $currFrame.Length) {
+        return $minFrame
+    }
+
+    FindMinRange `
+        -minFrame (CompareFramesGetMin $minFrame $currFrame) `
+        -currFrame ( `
+            [ArrayFrame]@{
+                Position = $currFrame.Position + 1
+                Sum = $currFrame.Sum - $range[$currFrame.Position] + $range[$currFrame.Position + $currFrame.Length]
+                Length = $currFrame.Length
+            })
+}
+
+function CompareFramesGetMin() {
+    Param(
+        [ArrayFrame]$frame1,
+        [ArrayFrame]$frame2
+    )
+
+    if ($frame1.Sum -lt $frame2.Sum) {
+        return $frame1
+    }
+
+    return $frame2
+}
+
+$range = InitRandomArray 20 10
+$subsetLen = 4
+
+$range
+
+Write-Host "`nSubset with the smallest sum:`n"
+FindMinRange `
+    -minFrame ([ArrayFrame]@{
+        Position = -1
+        Sum = [int]::MaxValue
+    }) `
+    -currFrame ([ArrayFrame]@{
+        Position = 0
+        Sum = CalcArraySum $range[0..($subsetLen - 1)]
+        Length = $subsetLen
+    }) | Out-Host
---
+++ SubarrayWithSmallestSum/README.txt
@@ -0,0 +1,14 @@
+Task:
+
+For a given array with N elements, find a subset of sequential M elements with smallest sum among subsets of the same lenght.
+
+Example
+
+Array of 10 Integer numbers
+3 1 6 6 8 2 5 2 9 0
+
+Find the subset of 3 elements with smallest sum
+
+Solution: 2 5 2
+
+Additional requirement: solution must not contain mutable variables. No variable value can be changed after assignment.
